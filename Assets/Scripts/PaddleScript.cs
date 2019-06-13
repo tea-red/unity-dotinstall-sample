8d606da5d9f95f7b8063ee1b99dc1888cd43ed21
tea-red <47109630+tea-red@users.noreply.github.com>
2019-06-14 07:11:23 +0900

    作成したファイルを一通り追加

---
+++ Assets/Scripts/PaddleScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleScript : MonoBehaviour
+{
+    // private float speed;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // speed = 5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(Input.GetAxis("Horizontal") * Time.deltaTime * speed, 0f, 0f);
+        // Debug.Log(transform.position.x);
+    }
+}
